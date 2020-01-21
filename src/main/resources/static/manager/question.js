493292a3ee9b9ec843e95b57421e693f7cd00c5d
brunohallef <brunohallef@gmail.com>
2020-01-20 23:21:08 -0200

    Enviando modificações

--- src/main/resources/static/manager/question.js
+++ src/main/resources/static/manager/question.js
@@ -1,8 +1,18 @@
 
+/* global Swal */
+
 var context;
 var qtdCasas;
 var numInicial;
 var selectNumeracao;
+var nextprofile;
+var nextmessage;
+var nextdoner;
+var nextdoacao;
+var codverificacao;
+var email;
+var pw;
+var createprofile;
 
 $(document).ready(function () {
     
@@ -10,12 +20,21 @@ $(document).ready(function () {
     qtdCasas = $("input[name='qtdcasas']");
     numInicial = $("input[name='numinicial']");
     selectNumeracao = $("select[name='numeracao']");
+    nextprofile = $("#nextprofile");
+    nextmessage = $("#nextmessage");
+    nextdoner = $("#nextdoner");
+    nextdoacao = $("#nextdoacao");
+    codverificacao = $("input[name='codigo']");
+    pw = $("input[name='senha']");
+    email = $("input[name='email']");
+    createprofile = $(".btn-new-profile");
     
     initCheck();
     evetCheck();
-    
     clearSelectNumCasas();
     numeracaoCondominioCasa();
+    next();
+    newProfile();
     
 });
 
@@ -40,19 +59,78 @@ function numeracaoCondominioCasa() {
 function clearSelectNumCasas() {
 
     qtdCasas.on("keyup", function () {
-        selectNumeracao.html("");
-        selectNumeracao.html("<option value='' label='Sequência das casas'></option>");
-        numInicial.val("");
-        if(Number(qtdCasas.val()) > 0) {
-            var options;
-            for (var i = 0; i < Number(qtdCasas.val()); i++) {
-                var numeracaoCasas = Number(numInicial.val()) + i;
-                options += "<option value='" + numeracaoCasas + "' label='Casa " + numeracaoCasas + "'></option>";
+        if(qtdCasas.val().trim().length <= 3) {
+            selectNumeracao.html("");
+            selectNumeracao.html("<option value='' label='Sequência das casas'></option>");
+            numInicial.val("");
+            if(Number(qtdCasas.val()) > 0) {
+                var options;
+                for (var i = 0; i < Number(qtdCasas.val()); i++) {
+                    var numeracaoCasas = Number(numInicial.val()) + i;
+                    options += "<option value='" + numeracaoCasas + "' label='Casa " + numeracaoCasas + "'></option>";
+                }
+                selectNumeracao.append(options);
             }
-            selectNumeracao.append(options);
+        } else {
+            qtdCasas.val(qtdCasas.val().substring(0,qtdCasas.val().length-1));
+            Swal.fire("Atenção!","Quantidade de casas deve estar entre 1 e 999","warning");
         }
     });
     
+}
+
+function newProfile() {
+    createprofile.click(function () {
+        
+        if(qtdCasas.val().trim() === "") {
+            $(".feed-quantidade").addClass("has-error has-feedback");
+            qtdCasas.focus();
+        } else {
+            $(".feed-quantidade").removeClass("has-error has-feedback");
+        }
+        
+        if(email.val().trim() === "") {
+            $(".feed-email").addClass("has-error has-feedback");
+            email.focus();
+        } else {
+            $(".feed-email").removeClass("has-error has-feedback");
+        }
+        
+        if(codverificacao.val().trim() === "") {
+            $(".feed-codigo-validacao").addClass("has-error has-feedback");
+            codverificacao.focus();
+        } else {
+            $(".feed-codigo-validacao").removeClass("has-error has-feedback");
+        }
+        
+        if(pw.val().trim() === "") {
+            $(".feed-senha").addClass("has-error has-feedback");
+            pw.focus();
+        } else {
+            $(".feed-senha").removeClass("has-error has-feedback");
+        }
+        
+    });
+}
+
+function next() {
+    
+    nextprofile.on("click", function () {
+        $("#href-home").removeClass("active");
+        $("#href-profile").addClass("active");
+        nextmessage.html("");
+        nextmessage.append("<li class='next'><a href='#message' data-toggle='tab'>Próximo <span aria-hidden='true'>&rarr;</span></a></li>");
+    });
+    
+    nextmessage.click(function () {
+        nextdoner.html("");
+        nextdoner.append("<li class='next'><a href='#doner' data-toggle='tab'>Próximo <span aria-hidden='true'>&rarr;</span></a></li>");
+    });
+    
+    nextdoner.click(function () {
+        nextdoacao.html("");
+        nextdoacao.append("<li class='next'><a href='#doacao' data-toggle='tab'>Próximo <span aria-hidden='true'>&rarr;</span></a></li>");
+    });
 }
 
 function evetCheck() {
@@ -63,9 +141,9 @@ function evetCheck() {
             $("#checkcqdlt").prop("checked", false).change();
             $("#checkceb").prop("checked", false).change();
             $("#checkmeb").prop("checked", false).change();
-            $("#href-profile").css("pointer-events", "");
+            nextprofile.append("<li class='next'><a href='#profile' data-toggle='tab'>Próximo <span aria-hidden='true'>&rarr;</span></a></li>");
         } else {
-            $("#href-profile").css("pointer-events", "none");
+            nextprofile.html("");
         }
     });
     
@@ -75,9 +153,9 @@ function evetCheck() {
             $("#checkcn").prop("checked", false).change();
             $("#checkceb").prop("checked", false).change();
             $("#checkmeb").prop("checked", false).change();
-            $("#href-profile").css("pointer-events", "");
+            nextprofile.append("<li class='next'><a href='#profile' data-toggle='tab'>Próximo <span aria-hidden='true'>&rarr;</span></a></li>");
         } else {
-            $("#href-profile").css("pointer-events", "none");
+            nextprofile.html("");
         }
     });
     
@@ -87,9 +165,9 @@ function evetCheck() {
             $("#checkcn").prop("checked", false).change();
             $("#checkcqdlt").prop("checked", false).change();
             $("#checkmeb").prop("checked", false).change();
-            $("#href-profile").css("pointer-events", "");
+            nextprofile.append("<li class='next'><a href='#profile' data-toggle='tab'>Próximo <span aria-hidden='true'>&rarr;</span></a></li>");
         } else {
-            $("#href-profile").css("pointer-events", "none");
+            nextprofile.html("");
         }
     });
     
@@ -99,13 +177,14 @@ function evetCheck() {
             $("#checkcn").prop("checked", false).change();
             $("#checkcqdlt").prop("checked", false).change();
             $("#checkceb").prop("checked", false).change();
-            $("#href-profile").css("pointer-events", "");
+            nextprofile.append("<li class='next'><a href='#profile' data-toggle='tab'>Próximo <span aria-hidden='true'>&rarr;</span></a></li>");
         } else {
-            $("#href-profile").css("pointer-events", "none");
+            nextprofile.html("");
         }
     });
 
 }
+
 
 
 function initCheck() {
